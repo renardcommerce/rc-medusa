f596d4975aea7e9070d45dddcf5ca9247abc20f5
renardcommerce <62641844+renardcommerce@users.noreply.github.com>
2026-05-01 20:56:02 +0200

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,10 +1,10 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS || 'http://localhost:8000',
       adminCors: process.env.ADMIN_CORS || 'http://localhost:9000',
